a3901cffa65ca66b7cb4bbe969d0e986c2a248e4
Haibo Xi <haibbo@gmail.com>
2023-04-04 22:43:58 +0800

    Add Cloudflare Worker script

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # cf-openai-azure-proxy
-A Cloudflare worker script to proxy OpenAI‘s request to Azure OpenAI Service
+
+> 大多数 OpenAI 客户端不支持 Azure OpenAI Service，但Azure OpenAI Service的申请和绑卡都非常简单，并且还提供了免费的额度。此脚本使用免费的 Cloudflare Worker 作为代理，使得支持 OpenAI 的客户端可以直接使用 Azure OpenAI Service。
+
+
+
+代理 OpenAI 的请求到 Azure OpenAI Serivce，代码部署步骤：
+
+1. 注册并登录到 Cloudflare 账户
+2. 创建一个新的 Cloudflare Worker
+3. 将 [cf-openai-azure-proxy.js](./cf-openai-azure-proxy.js) 复制并粘贴到 Cloudflare Worker 编辑器中
+4. 修改对应的 resourceName 和 deployName
+5. 保存并部署 Cloudflare Worker
+6. **必须**绑定自定义域名: 在 Worker 详情页 -> Trigger -> Custom Domains 中为这个 Worker 添加一个自定义域名
+
+### 使用说明
+
+第四步中的 resourceName 和 deployName, 必须要选对, 登录到Azure的后台:
+
+![azure](https://user-images.githubusercontent.com/1295315/229705215-e0556c99-957f-4d98-99a6-1c51254110b9.png)
+
+### 客户端 
+以 OpenCat 为例: 自定义 API 域名填写 第六步绑定的域名:
+
+<img width="339" src="https://user-images.githubusercontent.com/1295315/229820705-ab2ad1d1-8795-4670-97b4-16a0f9fdebba.png" alt="opencat" />
+
+我已经尝试了多种客户端, 如果遇到其他客户端有问题, 欢迎创建issue.
---
+++ cf-openai-azure-proxy.js
@@ -0,0 +1,98 @@
+// The name of your Azure OpenAI Resource.
+const resourceName="your-resource-name"
+
+// The deployment name you chose when you deployed the model.
+const deployName="deployment-name"
+
+const apiVersion="2023-03-15-preview"
+
+
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+  if (request.method === 'OPTIONS') {
+    return handleOPTIONS(request)
+  }
+
+  const url = new URL(request.url);
+  console.log(url.pathname)
+  if (url.pathname === '/v1/chat/completions') {
+    var path="chat/completions"
+  } else if (url.pathname === '/v1/completions') {
+    var path="completions"
+  } else if (url.pathname === '/v1/models') {
+    return handleModels(request)
+  } else {
+    return new Response('404 Not Found', { status: 404 })
+  }
+ 
+  const fetchAPI = `https://${resourceName}.openai.azure.com/openai/deployments/${deployName}/${path}?api-version=${apiVersion}`
+  let body;
+  if (request.method === 'POST') {
+    body = await request.json();
+  }
+  const authKey = request.headers.get('Authorization');
+  if (!authKey) {
+    return new Response("Not allowed", {
+      status: 403
+    });
+  }
+
+  const payload = {
+    method: request.method,
+    headers: {
+      "Content-Type": "application/json",
+      "api-key": authKey.replace('Bearer ', ''),
+    },
+    body: typeof body === 'object' ? JSON.stringify(body) : '{}',
+  };
+
+  const response = await fetch(fetchAPI, payload);
+  console.log(response.headers)
+  return response
+
+}
+
+async function handleModels(request) {
+  const data = {
+    "object": "list",
+    "data": [ {
+      "id": "gpt-3.5-turbo",
+      "object": "model",
+      "created": 1677610602,
+      "owned_by": "openai",
+      "permission": [{
+        "id": "modelperm-M56FXnG1AsIr3SXq8BYPvXJA",
+        "object": "model_permission",
+        "created": 1679602088,
+        "allow_create_engine": false,
+        "allow_sampling": true,
+        "allow_logprobs": true,
+        "allow_search_indices": false,
+        "allow_view": true,
+        "allow_fine_tuning": false,
+        "organization": "*",
+        "group": null,
+        "is_blocking": false
+      }],
+      "root": "gpt-3.5-turbo",
+      "parent": null
+    }]
+  };
+  const json = JSON.stringify(data, null, 2);
+  return new Response(json, {
+    headers: { 'Content-Type': 'application/json' },
+  });
+}
+
+async function handleOPTIONS(request) {
+    return new Response(null, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Allow-Headers': '*'
+      }
+    })
+}
